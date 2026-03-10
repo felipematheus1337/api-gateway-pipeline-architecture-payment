77f583ab5de6c2541b038ab3e018e8e0e76cd9e6
felipematheus1337 <lipehbr@gmail.com>
2026-03-10 10:56:19 -0300

    feature: finished DadosPagamentoController.

--- payment-pipeline-service/src/main/java/com/pipeline/service/infra/presentation/DadosPagamentoController.java
+++ payment-pipeline-service/src/main/java/com/pipeline/service/infra/presentation/DadosPagamentoController.java
@@ -1,12 +1,11 @@
 package com.pipeline.service.infra.presentation;
 
 import com.pipeline.service.application.CreateDadosPagamentoUseCase;
+import com.pipeline.service.domain.request.CreateDadosPagamentoRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/dados-pagamento")
@@ -16,7 +15,9 @@ public class DadosPagamentoController {
     private final CreateDadosPagamentoUseCase createDadosPagamentoUseCase;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<Void> create(@RequestBody CreateDadosPagamentoRequest request) {
-
+        createDadosPagamentoUseCase.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
